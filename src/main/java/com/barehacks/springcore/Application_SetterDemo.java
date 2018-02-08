91b10840715996c2378f5928ca3113fc560e45c1
barehacks <barehacks@asus-f1a55-m-lx3>
2018-02-09 00:41:12 +0800

    Configured the CricketCoach and spring config file to work on DI

---
+++ src/main/java/com/barehacks/springcore/Application_SetterDemo.java
@@ -0,0 +1,25 @@
+package com.barehacks.springcore;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application_SetterDemo {
+
+	public static void main(String[] args) {
+		//load the spring configuration file
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
+		
+		//retrieve the bean from the spring container
+		CricketCoach cricketCoach = context.getBean("myCricketCoach", CricketCoach.class);
+		
+		// call the method of the bean
+		System.out.println(cricketCoach.getDailyWorkout());
+		System.out.println(cricketCoach.getDailyFortune());
+		
+		//close the context
+		context.close();
+		
+
+	}
+
+}
